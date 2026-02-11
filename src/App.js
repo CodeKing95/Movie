7d778509e7b2f272e0312cb5d53d72cf8544de42
Nadir Ahmed <102181168+CodeKing95@users.noreply.github.com>
2026-02-11 15:29:51 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState } from "react"; 
 import styled from "styled-components";
 import MovieComponent from "./components/MovieComponent";
 import MovieInfoComponent from "./components/MovieInfoComponent";
